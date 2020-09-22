2b126155a439b99814af69130cadf7901ea19a8a
MaxStrikanov <maxvell.88@mail.ru>
2020-09-22 21:29:50 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import countTimer from './modules/countTimer';
+import toggleMenu from './modules/toggleMenu';
+import togglePopup from './modules/togglePopup';
+import tabs from './modules/tabs';
+import {slider, addDots} from './modules/slider';
+import changeImage from './modules/changeImage';
+import calc from './modules/calc';
+import sendForm from './modules/sendForm';
+
+countTimer('31 September 2020')
+
+toggleMenu();
+
+togglePopup();
+
+tabs();
+
+addDots();
+
+slider();
+
+changeImage();
+
+// calcValid();
+
+calc();
+
+sendForm();
